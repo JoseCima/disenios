2fcc51421279528675bc3db9e1358f1705ac6723
JoseCima <josegcima@gmail.com>
2022-02-25 14:46:49 -0500

    Diseño basico Parte 2

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -11,11 +11,24 @@ class BasicoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-            Image(
-              image: NetworkImage('https://plantcaretoday.com/wp-content/uploads/choosing-roses-garden-08312015-min.jpg'),
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+        ],
+      )
+    );
+  }
 
-            ),
-            Container(
+  Widget _crearImagen (){
+    return Image(
+              image: NetworkImage('https://live.staticflickr.com/65535/49168363698_337ea6289c_c.jpg'),
+              
+            );
+  }
+
+  Widget _crearTitulo(){
+    return Container(
               padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
               child: Row(
                 children: <Widget>[
@@ -36,9 +49,37 @@ class BasicoPage extends StatelessWidget {
 
                 ],
               ),
-            )
-        ],
-      )
+            );
+  }
+
+  Widget _crearAcciones(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'SHARE'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto){
+  return Column(
+          children: <Widget>[
+            Icon(icon, color: Colors.pink, size: 40.0),
+            SizedBox(height: 5.0),
+            Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.lightBlue),),
+          ],
+        );
+  }
+
+  Widget _crearTexto(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Mollit dolor ex ex aliqua excepteur. Ut occaecat et laborum ex ea dolor tempor tempor. Consectetur culpa laboris proident magna in aute. Elit quis est aliquip aliquip do esse ea.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
