72b1d5776e835eb48942df2499e85ccdec308a31
JoseCima <josegcima@gmail.com>
2022-02-25 15:29:28 -0500

    Diseño con Scroll - Parte 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:disenios/src/pages/basico_page.dart';
+import 'package:disenios/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,9 +11,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Diseños By JoseCima',
 
-     initialRoute: 'basico',
+     initialRoute: 'scroll',
      routes: {
        'basico'   : (BuildContext context) => BasicoPage(),
+       'scroll'   : (BuildContext context) => ScrollPage(),
+
 
      },
       
---
+++ lib/src/pages/scroll_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class ScrollPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          _pagina1(),
+          _pagina2(),
+        ],
+      ),
+
+    );
+  }
+
+  Widget _pagina1(){
+  return Stack(
+    children: <Widget>[
+      _colorFondo(),
+      _imagenFondo(),
+      _crearTextos(),
+    ],
+  );
+  }
+
+
+  Widget _crearTextos(){
+
+    final _estiloTexto = TextStyle(color: Colors.pink, fontSize: 50.0);
+   return SafeArea(
+     child: Column(
+       children: <Widget>[
+   
+         SizedBox(height: 20.0),
+         Text('11°', style: _estiloTexto),
+         Text('Miércoles', style: _estiloTexto),
+         Text('José Cima', style:TextStyle(color: Colors.white, fontSize: 30.0)),
+
+         Expanded( child: Container() ),//Expandiendo la flecha al bottom
+         Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.pink),
+   
+   
+       ],
+     ),
+   );
+  }
+
+
+Widget _imagenFondo(){
+  return Container(
+    width: double.infinity,
+    height: double.infinity,
+    child: Image(
+      image: AssetImage('assets/scroll.png'),
+      fit: BoxFit.cover,
+
+    ),
+  );
+}
+
+Widget _colorFondo(){
+  return Container(
+    width: double.infinity,
+    height: double.infinity,
+    color: Color.fromRGBO(108, 192, 2180, 1.0),
+
+  );
+}
+
+  
+
+
+
+  Widget _pagina2(){
+  return Center(
+      child: Text('Pagina 2 - JoseCima'),
+         );
+  }
+}
